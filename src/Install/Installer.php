9bd8dcc68933f00afb4bf843cc62f95cdb2962d5
Marijus Dilys <marijusdilys9@gmail.com>
2025-05-22 16:38:19 +0300

    [SL-316] Fix

--- src/Install/Installer.php
+++ src/Install/Installer.php
@@ -354,7 +354,7 @@ class Installer extends AbstractInstaller
             SaferPayConfig::SAFERPAY_PAYMENT_AUTHORIZED,
             'Payment authorized by Saferpay',
             '#4069e1',
-            true,
+            false,
             true,
             true,
             true,
